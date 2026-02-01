ac81a6819a4966edd8b125b3e3d7604480780da5
Hitesh lalwani <hlalwani28022001@gmail.com>
2026-02-01 23:38:11 +0530

    new array feature

---
+++ arrays.py
@@ -0,0 +1,9 @@
+def sum_of_arr(arr):
+    """
+    This function finds the sum of all numbers in an 
+    intiger array
+    """
+    sum = 0
+    for item in arr:
+        sum += item
+    return sum
